8ac6be1c69aa9acd180b1598782a93cabf95f72c
bouwehand <b.j.ouwehand@gmail.com>
2016-10-13 18:56:34 +0200

    Working on ing page

--- src/AppBundle/Controller/IngController.php
+++ src/AppBundle/Controller/IngController.php
@@ -1,10 +1,12 @@
 <?php
 // src/AppBundle/Controller/LuckyController.php
 namespace AppBundle\Controller;
-
+use ChocolateFactory\Csv;
+use Symfony\Component\Debug\DebugClassLoader;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Khill\Lavacharts\Lavacharts;
 
 class IngController extends Controller
 {
@@ -13,7 +15,9 @@ class IngController extends Controller
     */
     public function indexAction()
     {
+        Csv::init('sgsag');
         // replace this example code with whatever you need
+        $lava = new Lavacharts;
         return $this->render('ing/index.html.php');
     }
 }
